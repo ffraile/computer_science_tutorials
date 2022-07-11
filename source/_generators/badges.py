9dfa0a9aeea6039c720891665cda108a7d764bab
ffraile <francisco.fraile@mesview.com>
2022-07-11 10:32:44 +0200

    Fixed index Fixed problems in Introduction to Computer Programming.ipynb
    added applied mathematics folder and numpy tutorial

--- source/_generators/badges.py
+++ source/_generators/badges.py
@@ -17,8 +17,8 @@ if __name__ == '__main__':
         for template_file in pathlib.Path('../' + template_folder).glob('**/*.ipynb'):
             colabs_file_name = "/".join(template_file.parts[1:]).replace(" ", "%20")
             binder_file_name = "%2F".join(template_file.parts[1:]).replace(" ", "%20")
-            badges = "## Try me\n" +\
-                     " [![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/{0}/blob/{1}/{2}/{3})".format(git_repo, branch, path_to_template_folders, colabs_file_name) +\
+            badges = "## Try me\\n\"," +\
+                     "\"[![Open In Colab](https://colab.research.google.com/assets/colab-badge.svg)](https://colab.research.google.com/github/{0}/blob/{1}/{2}/{3})".format(git_repo, branch, path_to_template_folders, colabs_file_name) +\
                      "[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/{0}/{1}?labpath={2}%2F{3})".format(git_repo, branch, path_to_template_folders.replace("/","%2F"), binder_file_name)
 
             with open(template_file, 'r', encoding="utf8") as in_file:
