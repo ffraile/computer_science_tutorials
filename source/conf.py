2a186772a7d03cf6b46050df3fc92d86e4b3269a
Francisco <ffraile@cigip.upv.es>
2023-08-31 14:31:06 +0200

    switch theme to furo and install sphinx-design

--- source/conf.py
+++ source/conf.py
@@ -30,7 +30,9 @@ release = 'v0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['nbsphinx', 'myst_parser']
+extensions = ['nbsphinx', 'myst_parser', 'sphinx-design']
+myst_enable_extensions = ["colon_fence"]
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -53,7 +55,7 @@ master_doc = 'index'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'bizstyle'
+html_theme = 'furo'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
