045e890fa37c1a7ba3ea3eb005045dbcc6d0ba36
ffraile <francisco.fraile@mesview.com>
2023-09-01 12:40:15 +0200

    Changed ipynb parser to myst-nb

--- source/conf.py
+++ source/conf.py
@@ -34,7 +34,7 @@ html_logo = '_static/logo.png'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['nbsphinx', 'myst_parser', 'sphinx_design', 'sphinx_copybutton', 'sphinxext.opengraph']
+extensions = ['myst_parser', 'myst-nb', 'sphinx_design', 'sphinx_copybutton', 'sphinxext.opengraph']
 myst_enable_extensions = ["colon_fence", "myst_admonition_enable"]
 
 # Add any paths that contain templates here, relative to this directory.
